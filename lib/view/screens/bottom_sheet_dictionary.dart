ec574912ec4c931057029768f606135aaf5c52ce
thaiducpro <nguyenhuuthai.pro@gmail.com>
2025-01-13 22:10:34 +1100

    add ads to bottom sheet

--- lib/view/screens/bottom_sheet_dictionary.dart
+++ lib/view/screens/bottom_sheet_dictionary.dart
@@ -1,5 +1,6 @@
 import 'package:eng_dict/provider/word_field_data.dart';
 import 'package:eng_dict/view/screens/dictionary_screen.dart';
+import 'package:eng_dict/view/widgets/banner_ads_box.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
@@ -22,8 +23,15 @@ class BottomSheetDictionary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DictionaryScreen(
-      showAppBar: false,
+    return Column(
+      children: [
+        Expanded(
+          child: DictionaryScreen(
+            showAppBar: false,
+          ),
+        ),
+        const BannerAdsBox()
+      ],
     );
   }
 }
--- lib/view/widgets/word_title_box.dart
+++ lib/view/widgets/word_title_box.dart
@@ -13,7 +13,7 @@ class WordTitleBox extends StatelessWidget {
   WordTitleBox({
     required this.wordForm,
     super.key,
-  }) {}
+  });
 
   @override
   Widget build(BuildContext context) {
