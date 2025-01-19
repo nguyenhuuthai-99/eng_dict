1626857f0df82ee4b804ab593694916abc7879aa
thaiducpro <nguyenhuuthai.pro@gmail.com>
2025-01-19 23:06:45 +1100

    done fixing color

--- lib/view/utils/constants.dart
+++ lib/view/utils/constants.dart
@@ -64,19 +64,19 @@ class Constant {
   static const TextStyle kIPATextStyle = TextStyle(
       color: kHeading2Color, fontSize: 16, fontWeight: FontWeight.w100);
   static const TextStyle kUsageAndCodeTextStyle =
-      TextStyle(fontWeight: FontWeight.w100, color: kSecondaryColor);
+      TextStyle(fontWeight: FontWeight.w100, color: kContentTextColor);
   static TextStyle kAdditionalType = GoogleFonts.openSans(
-      fontWeight: FontWeight.w700, fontSize: 16, color: kPrimaryColor);
+      fontWeight: FontWeight.w700, fontSize: 16, color: kContentTextColor);
   static const kAdditionalTitle = TextStyle(
-      fontWeight: FontWeight.w700, fontSize: 15, color: kSecondaryColor);
+      fontWeight: FontWeight.w700, fontSize: 15, color: kPrimaryColor);
   static const kAdditionalJustification =
-      TextStyle(fontSize: 15, color: kSecondaryColor);
+      TextStyle(fontSize: 15, color: kContentTextColor);
   static const kAdditionalUsage = TextStyle(
       fontSize: 15, fontStyle: FontStyle.italic, color: kContentTextColor);
 
   static const kDot = TextSpan(
       text: "• ",
-      style: TextStyle(color: Constant.kSecondaryColor, fontSize: 18));
+      style: TextStyle(color: Constant.kPrimaryColor, fontSize: 18));
   static const kDotExample = TextSpan(
       text: "• ", style: TextStyle(color: Colors.black87, fontSize: 18));
 
