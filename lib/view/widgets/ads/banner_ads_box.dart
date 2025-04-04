4c22483062336664a0648dfa5310e4d971855970
thaiducpro <nguyenhuuthai.pro@gmail.com>
2025-04-05 06:00:20 +1100

    commit for release

--- lib/view/widgets/ads/banner_ads_box.dart
+++ lib/view/widgets/ads/banner_ads_box.dart
@@ -16,14 +16,14 @@ class _BannerAdsBoxState extends State<BannerAdsBox>
   late BannerAd bannerAd;
   bool isLoaded = false;
 
-  // String get adUnitId => Platform.isAndroid
-  //     ? 'ca-app-pub-2903872604687353/2000827477'
-  //     : 'ca-app-pub-2903872604687353/8979132933';
+  String get adUnitId => Platform.isAndroid
+      ? 'ca-app-pub-2903872604687353/2000827477'
+      : 'ca-app-pub-2903872604687353/8979132933';
 
   // //dummy ad
-  final adUnitId = Platform.isAndroid
-      ? 'ca-app-pub-3940256099942544/9214589741'
-      : 'ca-app-pub-3940256099942544/2435281174';
+  // final adUnitId = Platform.isAndroid
+  //     ? 'ca-app-pub-3940256099942544/9214589741'
+  //     : 'ca-app-pub-3940256099942544/2435281174';
 
   @override
   void didChangeDependencies() {
